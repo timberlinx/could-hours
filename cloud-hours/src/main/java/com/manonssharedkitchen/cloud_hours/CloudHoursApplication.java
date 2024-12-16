d2a1a29a60ce3d3b862bd581e001bc2f2e2d8202
nady <nady.obeid@gmail.com>
2024-12-16 14:48:36 -0800

    Initialize Springboot framework

---
+++ cloud-hours/src/main/java/com/manonssharedkitchen/cloud_hours/CloudHoursApplication.java
@@ -0,0 +1,13 @@
+package com.manonssharedkitchen.cloud_hours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudHoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudHoursApplication.class, args);
+	}
+
+}
---
+++ cloud-hours/src/test/java/com/manonssharedkitchen/cloud_hours/CloudHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manonssharedkitchen.cloud_hours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
