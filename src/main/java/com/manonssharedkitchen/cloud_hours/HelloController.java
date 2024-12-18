06b8997931f7f4dcff94a164c3260432ef37928b
timberlinx <nady.obeid@gmail.com>
2024-12-17 22:48:22 -0800

    Add Jackson for accepting json request body

--- src/main/java/com/manonssharedkitchen/cloud_hours/HelloController.java
+++ src/main/java/com/manonssharedkitchen/cloud_hours/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 public class HelloController {
@@ -14,9 +15,9 @@ public class HelloController {
 	}
 
 	@PostMapping("/brivologs")
-    public String handleIncomingBrivoLogs(@RequestBody String requestBody) {
+    public String handleIncomingBrivoLogs(@RequestBody JsonNode payload) {
         // Process the request body here
-        return "POST request received: " + requestBody;
+        return "POST request received: " + payload;
     }
 
 }
