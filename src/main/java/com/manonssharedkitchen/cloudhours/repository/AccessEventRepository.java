5dff73af5941fc7b0a67737bfe8fc8c6c3afa60f
nady <nady.obeid@gmail.com>
2024-12-19 13:40:43 -0800

    Removing unused code

--- src/main/java/com/manonssharedkitchen/cloudhours/repository/AccessEventRepository.java
+++ src/main/java/com/manonssharedkitchen/cloudhours/repository/AccessEventRepository.java
@@ -6,7 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccessEventRepository extends JpaRepository<AccessEvent, Integer> {
-
-    // Find users by name
-//    List<AccessEvent> findByAccessEventId(String accessEventId);
 }
